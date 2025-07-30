076c6f5d5b6830b363299468c05cf71d4ae3e343
Samuel Ward <samuel@infernal.dev>
2025-07-30 18:15:41 +0200

    feat: posthog rewrite

--- src/composables/usePostHog.ts
+++ src/composables/usePostHog.ts
@@ -2,9 +2,10 @@ import posthog from "posthog-js";
 
 export const usePostHog = () => {
   posthog.init(import.meta.env.VITE_POSTHOG_TOKEN, {
-    api_host: import.meta.env.VITE_POSTHOG_HOST,
+    api_host: import.meta.env.VITE_POSTHOG_API_HOST,
+    ui_host: "https://eu.posthog.com",
     defaults: "2025-05-24",
-    person_profiles: "always",
+    person_profiles: "identified_only",
   });
 
   return { posthog };
